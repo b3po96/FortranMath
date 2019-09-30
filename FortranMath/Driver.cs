fcded68da422813e1bd004b58c9be3077759983f
Bryce Buchanan <Bryce.C.Buchanan-1@ou.edu>
2019-09-30 17:00:54 -0500

    Converts driver program to Windows Form

--- FortranMath/Driver.cs
+++ FortranMath/Driver.cs
@@ -1,14 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace FortranMath
 {
-    class Driver
+    class Driver:System.Windows.Forms.Form
+    
     {
+        Driver()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.ClientSize = new System.Drawing.Size(367, 188);
+            this.Name = "FortranMath";
+            this.ResumeLayout(false);
+        }
 
         [DllImport("FortranFunc.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
             static extern void StrassenAlgorithm();
@@ -16,8 +31,11 @@ namespace FortranMath
         [DllImport("FortranFunc.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
             static extern void EratosAlgorithm();
 
-        static void Main(string[] args)
+        static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Driver());
             while (true)
             {
                 Console.WriteLine("What would you like to do?");
